15f80ea2062b622f699cde29a4873f40fba2aa58
M-Abdulrhman-Alomari <abdulrhmanalomaricv@gmail.com>
2025-12-21 11:56:34 -0800

    fix Faker problem

--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -4,18 +4,22 @@ namespace Database\Seeders;
 
 use App\Models\Region;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class RegionSeeder extends Seeder
 {
     public function run(): void
     {
+        $faker = Faker::create(); // مولّد بيانات وهمية
+
         foreach (['Al-Mazzeh','Barzeh','Al-Maliki','Kafr Sousa','Al-Marjeh'] as $name) {
             Region::firstOrCreate(
                 ['name' => $name],
-                ['latitude' => fake()->randomFloat(8, 33.40, 33.60),
-                 'longitude'=> fake()->randomFloat(8, 36.15, 36.40)]
+                [
+                    'latitude'  => $faker->randomFloat(8, 33.40, 33.60),
+                    'longitude' => $faker->randomFloat(8, 36.15, 36.40),
+                ]
             );
         }
-        // أو Region::factory()->count(5)->create(); إذا الFactory يضمن unique('name')
     }
 }
